e8d36373a28af66bcbfaa1ada3a8ff5922e5d7f9
Sean Gatewood <stg2bd@virginia.edu>
2019-02-17 12:42:15 -0500

    Created simple "Listing" model

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.IntegerField()
+
+    def __str__(self):
+        return name
