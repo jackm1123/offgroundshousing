a65e5e3299aa3ebd2488f11dd1a093d98650a45a
Sean Gatewood <stg2bd@virginia.edu>
2019-02-17 13:00:55 -0500

    Registered Listing in admin.py
    Also fixed bug in models.py (self.name)

--- listings/models.py
+++ listings/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
 class Listing(models.Model):
+    """
+    TODO: Add other listing fields, like location, square footage, etc.
+    Don't forget to makemigrations.
+    """
     name = models.CharField(max_length=100)
     rating = models.IntegerField()
 
     def __str__(self):
-        return name
+        return self.name
