a4c5c50557eb30de6ce3d7dec2f130afce2b0c66
Sean Gatewood <stg2bd@virginia.edu>
2019-02-17 13:42:23 -0500

    Made listings JSON strings (just an idea, might not use)

--- listings/models.py
+++ listings/models.py
@@ -9,4 +9,21 @@ class Listing(models.Model):
     rating = models.IntegerField()
 
     def __str__(self):
-        return self.name
+        return dict_to_json(self.__dict__)
+        # return str(self.__dict__)
+
+def dict_to_json(d):
+    copy = d.copy()
+    out = "{"
+
+    for key in copy:
+        out += '"' + key + '":'
+        value = copy[key]
+        if(type(value) == int):
+            out += str(value) + ","
+        else:
+            out += '"' + str(value) + '",'
+
+    out = out[:-1] # chop off last comma
+    out += "}"
+    return out
