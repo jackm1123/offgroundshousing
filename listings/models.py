9c1b2200fa96ee95edf96422500a42600d98adb7
Jack McManus <jackmcmanus2@gmail.com>
2019-02-20 22:46:52 -0500

    updated models and bootstrap

--- listings/models.py
+++ listings/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from datetime import datetime
 
 class Listing(models.Model):
-    """
-    TODO: Add other listing fields, like location, square footage, etc.
-    Don't forget to makemigrations.
-    """
+
     name = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
+    description = models.TextField(default="")
+    submission_date = models.DateTimeField(default=datetime.now, blank=True)
+    LAUNDRY_CHOICES = [('L', 'Laundry'), ('N', 'No Laundry')]
+    #The first element in each tuple is the value that will be stored in the database. The second element is displayed by the field’s form widget.
+    laundry_info = models.CharField(choices=LAUNDRY_CHOICES, max_length=1, blank = False, default='N') # laundry required field
+    square_footage = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    bedroom_num = models.IntegerField(default=0)
+    phone_num = models.CharField(max_length=20, default=0)
+    address = models.CharField(max_length=100, default="")
+    OWNERSHIP_CHOICES = [('O', 'Owned'), ('A', 'Available')]
+    ownership_info = models.CharField(choices=OWNERSHIP_CHOICES, max_length=1, blank = False, default='A') # status is required field
+
+
+
 
     def get_json(self):
         return dict_to_json(self.__dict__)
--- segfaulters/settings.py
+++ segfaulters/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listings.apps.ListingsConfig'
+    'listings.apps.ListingsConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
