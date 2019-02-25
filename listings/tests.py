4decd7dcab7e9c0cd1e8c8b42ce4e2ea114ba5ed
Sean Gatewood <stg2bd@virginia.edu>
2019-02-24 20:38:37 -0500

    Added the first test

--- listings/tests.py
+++ listings/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Listing
+
+class ListingTest(TestCase):
+
+	def setUp(self):
+		pass
+
+	def test_positional_arguments(self):
+		l = Listing("id","name","rating","description","submission_date","laundry_info","square_footage","price","bedroom_num","phone_num","address","ownership_info")
+		for item in l.__dict__:
+			if item != "_state":
+				self.assertEqual(item,l.__dict__[item],"Listing Constructor: Change detected in positional arguments (name or order). \nThrown for: Listing." + str(item))
+		# l.print_details() # Uncomment to see how things fell into place
