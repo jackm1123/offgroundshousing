ed1f736e9352ea3e344bad47a2c97ffb2c371a4e
Jack McManus <jackmcmanus2@gmail.com>
2019-02-26 01:02:32 -0500

    take2

--- segfaulters/settings.py
+++ segfaulters/settings.py
@@ -21,7 +21,13 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_URL = '/static/'
+# define where your static files will be collected
+# It will be `absolute/path/to/demo3/static`
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# keep it empty for the moment
+STATICFILES_DIRS = (
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
