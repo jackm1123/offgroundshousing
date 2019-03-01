93ad30c05dcb6045bfd811ccf053d99725be2f16
Sean Gatewood <stg2bd@virginia.edu>
2019-03-01 13:02:32 -0500

    test

--- segfaulters/settings.py
+++ segfaulters/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'listings.apps.ListingsConfig',
-    'crispy_forms',
+    'crispy_forms', #dont have,
 ]
 
 MIDDLEWARE = [
