f71a0c11d5ce10a566bb100a80ef6a9484c899a4
Sean Gatewood <stg2bd@virginia.edu>
2019-03-02 00:45:22 -0500

    Whoops, forgot get_queryset is mandatory

--- listings/views.py
+++ listings/views.py
@@ -7,3 +7,6 @@ from .models import Listing
 class IndexView(generic.ListView):
     template_name = 'listings/list_of_listings.html'
     context_object_name = 'list_of_listings'
+
+    def get_queryset(self):
+        return Listing.objects.all()
