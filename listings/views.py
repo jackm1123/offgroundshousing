3b4f03fc3ba43ae0fb139c2d0d2bc074e72c9c45
Sean Gatewood <stg2bd@virginia.edu>
2019-03-01 21:06:27 -0500

    Removed dummy view

--- listings/views.py
+++ listings/views.py
@@ -7,6 +7,3 @@ from .models import Listing
 class IndexView(generic.ListView):
     template_name = 'listings/list_of_listings.html'
     context_object_name = 'list_of_listings'
-
-def dummy(request):
-    return HttpResponse("<h1>Congrats, you reached the dummy view</h1>")
