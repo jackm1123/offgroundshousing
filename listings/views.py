f2da961829272480c8bbc2cc136c1a9d1816d1fb
djx7et <djx7et@virginia.edu>
2019-03-03 18:36:04 -0500

    making homepage visible

--- listings/views.py
+++ listings/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from .models import Listing
 
 class IndexView(generic.ListView):
@@ -13,3 +14,6 @@ class IndexView(generic.ListView):
 
 def dummy(request):
     return HttpResponse("<h1>Congrats, you reached the dummy view</h1>")
+
+def index(request):
+    return render_to_response('home/index.html', context_instance=RequestContext(request))
