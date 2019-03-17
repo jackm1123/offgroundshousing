71c16a899004c65bea0bf60a158cad74fe6194c3
Sean Gatewood <stg2bd@virginia.edu>
2019-03-17 17:48:40 -0400

    Sean and Farid

--- listings/urls.py
+++ listings/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'listings'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path("<int:listing_id>/",views.one_listing, name="one_listing")
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404, render
 from django.template import RequestContext
-
 from .models import Listing
 
 class IndexView(generic.ListView):
@@ -12,6 +11,13 @@ class IndexView(generic.ListView):
 
     def get_queryset(self): # pragma no cover (not sure how to test)
         return Listing.objects.all()
+
+def one_listing(request,listing_id):
+    listing = get_object_or_404(Listing,pk=listing_id)
+    context = {
+        "listing" : listing,
+    }
+    return render(request, 'listings/one_listing.html', context)
 
 # Deleted: I think this code isn't used
 # def index(request):
