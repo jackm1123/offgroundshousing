d283125f9ef1c3420c6533ce0bcd46f0e01b3a45
Sean Gatewood <stg2bd@virginia.edu>
2019-03-23 21:21:33 -0400

    Made navbar reside in base.html

--- listings/urls.py
+++ listings/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'listings'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.IndexView.as_view(), name='list_of_listings'),
     path("<int:listing_id>/",views.one_listing, name="one_listing")
 ]
