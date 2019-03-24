e0c3e50f103130790598844dd209d26c6f9bf878
Sean Gatewood <stg2bd@virginia.edu>
2019-03-24 05:35:35 -0400

    Embeddable carousel working

--- listings/views.py
+++ listings/views.py
@@ -26,6 +26,13 @@ def one_listing_condensed(request,listing_id):
     }
     return render(request, 'listings/one_listing_condensed.html', context)
 
+def one_listing_slides(request,listing_id):
+    listing = get_object_or_404(Listing,pk=listing_id)
+    context = {
+        "listing" : listing,
+    }
+    return render(request, 'listings/one_listing_slides.html', context)
+
 # Deleted: I think this code isn't used
 # def index(request):
 #     return render_to_response('home/index.html')
