8e31191ddb701e8f24083f1539f8da5607e7bc4c
Sean Gatewood <stg2bd@virginia.edu>
2019-03-24 06:41:26 -0400

    Minor bug fixes

--- listings/static/listings/rating.js
+++ listings/static/listings/rating.js
@@ -1,18 +1,17 @@
-$(function() {
-
-  var rating_p = $(".rating").children("p");
-  rating_p.hide();
-  var rating = parseInt(rating_p.text().split("=")[1]);
+function hide_stars(rating){
+  var rating = parseInt(rating);
 
   var star_images = $(".rating").children("img");
   star_images.each(function(){
     var star_num = parseInt($(this).attr("num"));
-    if(star_num != rating){
-      $(this).hide();
-    }else{
-      $(this).show();
+    if(!$(this).hasClass("done")){
+      if(star_num != rating){
+        $(this).hide();
+      }else{
+        $(this).show();
+      }
+      $(this).addClass("done");
     }
+
   });
-
-
-});
+}
