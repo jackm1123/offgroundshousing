b4d6c26d0d06efaab1b59ebd6f5171311bda8f9b
Jack McManus <jackmcmanus2@gmail.com>
2019-04-07 17:30:33 -0400

    added some search functionality

--- segfaulters/views.py
+++ segfaulters/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.http import HttpResponse
+from django.apps import apps
+listing = apps.get_model('listings', 'Listing')
 
 
 def home(request):
@@ -8,3 +11,14 @@ def home(request):
 
 def profile(request):
     return render(request, 'userprofile.html')
+
+def search(request):
+    if 'query' in request.GET and request.GET['query']:
+        q = request.GET['query']
+        q = q.strip()
+        props = listing.objects.filter(name__icontains=q)
+        return render(request, 'listings/list_of_listings.html',
+                      {'list_of_listings': props, 'query': q})
+
+    else:
+        return HttpResponse('None Found Sorry')
