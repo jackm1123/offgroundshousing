b9d612d61996914c3ba1d5de973eed3067f4d2d5
Sean Gatewood <stg2bd@virginia.edu>
2019-04-08 07:14:48 -0400

    Working on making dashboard

--- users/urls.py
+++ users/urls.py
@@ -3,3 +3,9 @@ from django.urls import path
 from . import views
 
 app_name = 'users'
+
+urlpatterns = [
+    path('', views.usr, name='usr'),
+    path('dashboard', views.dashboard, name='dashboard'),
+
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response, get_object_or_404, render
+
 
 # Create your views here.
+
+def dashboard(request):
+    return render(request, 'users/dashboard.html',{})
+
+def usr(request):
+    return render(request,'users/usr.html',{})
