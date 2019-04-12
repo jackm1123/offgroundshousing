3e8d60d8e5a038a8ae3fe06a3114e2d59d184123
Sean Gatewood <stg2bd@virginia.edu>
2019-04-12 16:11:43 -0400

    quick snapshot before pull

--- listings/models.py
+++ listings/models.py
@@ -102,6 +102,11 @@ class Listing_Image(models.Model):
     listing = models.ForeignKey(Listing, related_name='images',on_delete=models.CASCADE)
     image = models.ImageField(blank=True, null=True, upload_to=("listing_pics/"))
 
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, related_name='reviews',on_delete=models.CASCADE)
+    body = models.TextField(default="")
+
+
 def dict_to_json(d):
     copy = d.copy()
     out = "{"
