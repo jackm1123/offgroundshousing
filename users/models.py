0b231a53a4d6078adffd555721ff31804a1207c2
Sean Gatewood <stg2bd@virginia.edu>
2019-04-12 05:15:12 -0400

    Create User profiles on login

--- users/models.py
+++ users/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+
+import shutil # https://stackoverflow.com/questions/1534986/how-do-i-override-delete-on-a-model-and-have-it-still-work-with-related-delete
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+from django.apps import apps
+
 # from listings.models import Listing
 # import django.db.models
 # from django.apps import apps
 # MyModel1 = apps.get_model('listings', 'Listing')
+
+# from django.contrib.auth.signals import user_logged_in
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -24,6 +33,19 @@ class UserProfile(models.Model):
             if u.user.username == username:
                 return u
 
+# @receiver(pre_delete)
+# def delete_profile(sender, instance, **kwargs):
+#     if sender == UserProfile:
+#         instance.user.delete()
+        # Listing = apps.get_model('listings', 'Listing')
+        # for l in Listing.objects.all():
+        #     if(instance in )
+
+# def do_stuff(sender, user, request, **kwargs): #https://stackoverflow.com/questions/1990502/django-signal-when-user-logs-in
+#     whatever...
+
+
+
 
 # class LeasingAgent(UserProfile):
 
@@ -34,6 +56,10 @@ class UserProfile(models.Model):
 #Create UserProfile upon new User signing in
 # https://github.com/maxg203/Django-Tutorials/blob/master/accounts/models.py
 def create_profile(sender, **kwargs):
-    if kwargs['created']:
+    user = kwargs['instance']
+    if UserProfile.get_user(user.username) == None:
         user_profile = UserProfile.objects.create(user=kwargs['instance'])
-    post_save.connect(create_profile, sender=User)
+
+post_save.connect(create_profile, sender=User)
+
+# user_logged_in.connect(create_profile)
