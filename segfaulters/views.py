9c579e8131eea81c60e55d6a8c9bcd4380518aaf
Sean Gatewood <stg2bd@virginia.edu>
2019-04-12 17:04:06 -0400

    userprofile working

--- segfaulters/views.py
+++ segfaulters/views.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.apps import apps
 Listing = apps.get_model('listings', 'Listing')
+UserProfile = apps.get_model('users', 'UserProfile')
 
 
 
@@ -10,8 +11,9 @@ def home(request):
 	return render(request, 'home.html')
 
 def profile(request):
-	objects = Listing.objects.all()
-	return render(request, 'userprofile.html', {'list_of_listings': objects})
+    objects = Listing.objects.all()
+    profile = UserProfile.get_user(request.user.username)
+    return render(request, 'userprofile.html', {'list_of_listings': objects,'UserProfile' : profile})
 
 def contact(request):
     return render(request, 'contact.html')
